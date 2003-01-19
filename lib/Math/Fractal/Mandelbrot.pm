1cc61de605a7d8daa7f221f1abeb082a15451f1f
Tels <nospam-abuse@bloodgate.com>
2003-01-19 16:17:24 +0000

    Import of TELS/Math-Fractal-Mandelbrot-0.02 from CPAN.
    gitpan-cpan-distribution: Math-Fractal-Mandelbrot
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TELS/math/Math-Fractal-Mandelbrot-0.02.tar.gz
    gitpan-cpan-author:       TELS
    gitpan-cpan-maturity:     released

--- lib/Math/Fractal/Mandelbrot.pm
+++ lib/Math/Fractal/Mandelbrot.pm
@@ -13,13 +13,16 @@ require DynaLoader;
 use vars qw/@ISA $VERSION @EXPORT_OK/;
 @ISA = qw(Exporter DynaLoader);
 
-@EXPORT_OK = qw/point/;
+@EXPORT_OK = qw/
+   point hor_line ver_line
+   set_bounds set_limit set_max_iter set_epsilon
+   /;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 bootstrap Math::Fractal::Mandelbrot $VERSION;
 
-# no Perl code, it is all in the XS code
+# no Perl code, it's all in the XS code
 
 1;
 __END__
@@ -39,6 +42,84 @@ famous Mandelbrot fractal.
  
 This program is free software; you may redistribute it and/or modify it under
 the same terms as Perl itself. 
+
+=head1 METHODS
+
+=over 2
+
+=item set_max_iter()
+	
+	Math::Fractal::Mandelbrot->set_max_iter($max_iter);
+
+Set the maximum number of iterations. 600 is the default and quite suitable
+for the start image. When zooming in, this value should be increased to
+not loose details.
+
+=item set_limit()
+
+	Math::Fractal::Mandelbrot->set_limit($limit);
+
+The default value is 5 and should only be changed if you know why.
+
+=item set_epsilon()
+
+	Math::Fractal::Mandelbrot->set_epsilon($e);
+
+The default value is 0.001. When the change between two iterations is less
+than this number, the point is considered to be on the inside.
+
+=item set_bounds()
+
+	Math::Fractal::Mandelbrot->set_bounds($x1,$y1,$x2,$y2,$w,$h);
+
+Set the coordinates from x1, y1 to x2, y2 and the width of the computed image
+to w and h. 
+
+The default values are:
+
+	Math::Fractal::Mandelbrot->set_bounds(-2,-1.1, 1,1.1, 640,480);
+
+=item point()
+
+	my $iter = Math::Fractal::Mandelbrot->point($x,$y);
+
+Calculates the value at the point C<$x> and C<$y>. The return value 0 means
+the point is inside the fractal (typical the black area), any value >0 means
+the number of iterations it took to find out that the point is on the outside.
+
+$x and $y should be between 0 and C<w> and 0 and C<h>, respectively (see
+L<set_bounds()>).
+
+=item hor_line($x1,$y1,$l)
+
+	my $points = Math::Fractal::Mandelbrot->hor_line($x1,$y1,$l);
+
+Calculates the values at a horizontal line and returns them all as a ref to
+am array.
+
+The array will contain one extra value, which is the count of values
+in the array beeing equal to the first value. Example:
+
+
+	values		count	explanation
+	==============================================================
+	1,1,1,1,3,	4 	# 4 are equal
+	1,1,3,1,3,	2	# only 2, not 3 since it stops after 2
+
+See L<point()> for details.
+
+=item ver_line($x1,$y1,$l)
+
+	my @points = Math::Fractal::Mandelbrot->ver_line($x1,$y1,$l);
+
+Calculates the values at a vertical line and returns them all as array.
+The array will contain one extra value, which is the count of values
+in the array beeing equal to the first value. See  L<hor_line> for
+an explanation of this last value.
+
+See L<point()> for further details.
+
+=head1 AUTHOR
 
 =head1 AUTHOR
 
--- t/mandel.t
+++ t/mandel.t
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-use Test::More tests => 4;
+use Test::More tests => 12;
 use strict;
 
 BEGIN
@@ -8,7 +8,7 @@ BEGIN
   $| = 1;
   unshift @INC, '../blib/lib';
   unshift @INC, '../blib/arch';
-  unshift @INC, '.';
+  # unshift @INC, '.';
   chdir 't' if -d 't';
   use_ok ('Math::Fractal::Mandelbrot');
   }
@@ -16,9 +16,30 @@ BEGIN
 my $c = 'Math::Fractal::Mandelbrot';
 
 can_ok ($c, qw/ 
-  point
+   point
+   hor_line ver_line
+   set_bounds set_limit set_max_iter set_epsilon
   /);
 
-is ($c->point (0,0, 100, 5), 0, '0,0 is in the set');
-is ($c->point (-2,-2, 100, 5), 1, '0,0 is not in the set');
+is ($c->point (320,200), 0, '0,0 is in the set');
+is ($c->point (0,0), 1, '-2,-2 is not in the set');
+
+is ($c->point (0,0), 1, '-2,-2 is not in the set');
+
+is ($c->set_max_iter(800), 800, 'can set max_iter');
+is ($c->set_epsilon(0.01), 0.01, 'can set epsilon');
+is ($c->set_limit(25), 25, 'can set limt');
+
+my $line;
+$line = $c->hor_line (0,0,10);
+is (scalar @$line, 11, 'got 11 values for hor');
+is ($line->[-1], 10, 'got 10 equal values for hor');
+
+use Data::Dumper; print Dumper $line;
+
+$line = $c->ver_line (0,0,10);
+is (scalar @$line, 11, 'got 11 values for ver');
+is ($line->[-1], 10, 'got 10 equal values for ver');
+
+use Data::Dumper; print Dumper $line;
 
